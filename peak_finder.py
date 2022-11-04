2b949c3f0575a57d7e2eeb9fa560a9a5614e0545
StefanoVergani <s.vergani@ucl.ac.uk>
2022-11-04 15:08:55 +0000

    small details changed

--- peak_finder.py
+++ peak_finder.py
@@ -16,5 +16,5 @@ import statistics
 path_dir: str = r"/unix/dune/purity/2022October14Vacuum/Gold/"
 path_dir_output: str =r"/home/svergani/waveform_study_results/2022_10_31/"
 #the third argument is polarity. It is +1 if the waveform has a positive peak, -1 if negative.
-ppf.peak_finder_ave(path_dir,".ch3.traces",1,path_dir_output,4)
-#ppf.peak_finder_ave(path_dir,".ch4.traces",-1,path_dir_output,20)
+ppf.peak_finder_ave(path_dir,".ch3.traces",1,path_dir_output,20)
+ppf.peak_finder_ave(path_dir,".ch4.traces",-1,path_dir_output,20)
