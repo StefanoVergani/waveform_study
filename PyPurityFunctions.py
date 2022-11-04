0c0c8f7ac6cb9e720ec197f4a4555ba59f750d34
StefanoVergani <s.vergani@ucl.ac.uk>
2022-11-04 15:07:19 +0000

    a perfectly working version

--- PyPurityFunctions.py
+++ PyPurityFunctions.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import statistics
 import time
+from datetime import datetime
+import pickle
 from PyPurityTools import PyPurityTools as ppt
 
 
@@ -41,27 +43,40 @@ class PyPurityFunctions:
         return mega_average
 
     @staticmethod            
-    def peak_finder_ave(path_raw,file_ending,polarity):
+    def peak_finder_ave(path_raw,file_ending,polarity,path_output,file_size):
         peaks = []
+        counter = 0
         directory = os.fsencode(path_raw)
         for file in os.listdir(directory):
-            start_time = time.time()
             filename = os.fsdecode(file)
             peak = 0
             if filename.startswith("Field_5.10.20Vcm_FibreIn_") and filename.endswith(file_ending):
+                print(counter)
                 waveList, timeList = ppt.getScopeWaveforms(path_raw+filename)
+                timeList = None
                 max_list = []
                 for i in range(len(waveList)):  
                     if(polarity==1):
                         max_list.append(max(waveList[i]))
                     if(polarity==-1):
                         max_list.append(min(waveList[i]))
+                        waveList[i]=None
+                waveList = None
                 peak = statistics.mean(max_list)
                 max_list.clear()
-                waveList = None
-                timeList = None
-                peaks.append(peak)    
-        return peaks
+                counter = counter +1
+                peaks.append(peak)
+                if counter == file_size and polarity==1:
+                    print("I am now printing file ch3_peaks_"+"{:%Y_%m_%d_%H_%M_%S}".format(datetime.now())+".npy")
+                    with open(path_output+"ch3_peaks_"+"{:%Y_%m_%d_%H_%M_%S}".format(datetime.now())+".npy",'wb') as f: pickle.dump(np.array(peaks), f)
+                    counter=0
+                    peaks.clear()
+                if counter == file_size and polarity==-1:
+                    print("I am now printing file ch4_peaks_"+"{:%Y_%m_%d_%H_%M_%S}".format(datetime.now())+".npy")
+                    with open("/home/svergani/waveform_study_results/2022_10_31/ch4_peaks_"+"{:%Y_%m_%d_%H_%M_%S}".format(datetime.now())+".npy",'wb') as f: pickle.dump(np.array(peaks), f)
+                    counter=0
+                    peaks.clear()
+        #return peaks
     
     @staticmethod            
     def specific_file_counter(path_raw,file_ending):
@@ -73,3 +88,20 @@ class PyPurityFunctions:
             if filename.startswith("Field_5.10.20Vcm_FibreIn_") and filename.endswith(file_ending):
                 counter = counter+1             
         return counter
+    
+    #I loop over all the files in path_dir_output
+    @staticmethod            
+    def file_collector(path_raw,file_type):
+        peaks = []
+        directory = os.fsencode(path_raw)
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            if filename.startswith(file_type) and filename.endswith(".npy"):
+                print("filename ", filename)
+                temp = []
+                with open(path_raw+filename, "rb") as fp:   #Pickling
+                    temp=pickle.load(fp)
+                for i in range(len(temp)):
+                    peaks.append(temp[i])
+                temp= None
+        return peaks
---
+++ peak_finder.py
@@ -0,0 +1,20 @@
+from PyPurityTools import PyPurityTools as ppt
+from PyPurityFunctions import PyPurityFunctions as ppf
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from scipy.signal import savgol_filter
+import matplotlib.style
+import matplotlib as mpl
+import math
+import scipy.fftpack
+from scipy.signal import butter,filtfilt
+import scipy.fftpack
+import os
+import statistics
+
+path_dir: str = r"/unix/dune/purity/2022October14Vacuum/Gold/"
+path_dir_output: str =r"/home/svergani/waveform_study_results/2022_10_31/"
+#the third argument is polarity. It is +1 if the waveform has a positive peak, -1 if negative.
+ppf.peak_finder_ave(path_dir,".ch3.traces",1,path_dir_output,4)
+#ppf.peak_finder_ave(path_dir,".ch4.traces",-1,path_dir_output,20)
